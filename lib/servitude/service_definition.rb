e65b2c1d0e6bc841074a8a6b194a264dbe68a1c6
Chris Rohr <rohr.chris@gmail.com>
2016-11-20 18:33:43 -0500

    Added the service definition model
    Fixes #2

--- lib/servitude/service_definition.rb
+++ lib/servitude/service_definition.rb
@@ -1,11 +1,23 @@
 module Servitude
-    class ServiceDefinition
-       attr_accessible :name, :description, :language, :datastores, :message_types_consumed, :message_types_produced, :rest_dependencies
-       
-       def self.from(attributes)
-           if !attributes.respond_to?(:stringify_keys)
-               raise ArgumentError, "When assigning attributes, you must pass a hash as an argument."
-           end
-       end
+  class ServiceDefinition
+    attr_accessor :name,
+                  :description,
+                  :language,
+                  :datastores,
+                  :message_types_consumed,
+                  :message_types_produced,
+                  :rest_dependencies
+
+    def self.from(attributes)
+      definition = ServiceDefinition.new
+
+      return definition if attributes.nil?
+
+      attributes.each do |k, v|
+        definition.send("#{k}=", v) if definition.respond_to?("#{k}=")
+      end
+
+      definition
     end
+  end
 end
