465a712e859f00fead4426f88906cc984bacd717
Chris Rohr <rohr.chris@gmail.com>
2016-11-21 00:21:28 -0500

    Added in the base cli with options
    Options for:
    * Locating definition files
    * Output
    * Verbosity
    * Help
    
    Fixes #3

--- lib/servitude.rb
+++ lib/servitude.rb
@@ -1,4 +1,5 @@
 require 'servitude/version'
+require 'servitude/service_definition'
 
 module Servitude
   # Your code goes here...
--- lib/servitude/cli.rb
+++ lib/servitude/cli.rb
@@ -4,7 +4,54 @@ require 'optparse'
 module Servitude
   class Cli
     def run
-      puts 'Wooo we are running!'
+      options = parse_options
+      defs = load_defs(options)
+    end
+
+    private
+
+    def parse_options
+      options = {}
+      OptionParser.new do |opts|
+        opts.banner = 'Usage: servitude [options]'
+
+        opts.on('-d DIRECTORY', '--directory=DIRECTORY', 'Locate definition files in given directory') do |d|
+          options[:directory] = d
+          options[:location_type] = :directory
+        end
+
+        opts.on('-g REPOSITORIES',
+                '--group-of-repositories=REPOSITORIES',
+                'Locate definition files across many git repositories (SERVITUDE files)') do |g|
+          options[:group] = g
+          options[:location_type] = :group
+        end
+
+        opts.on('-h', '--help', 'Prints this help') do
+          puts opts
+          exit
+        end
+
+        opts.on('-o OUTPUTTYPE', '--output-type=OUTPUTTYPE', 'Format of output (SVG, PNG, Text, HTML)') do |o|
+          options[:output] = o
+        end
+
+        opts.on('-s REPOSITORY',
+                '--single-repository=REPOSITORY',
+                'Locate all definition files in a single git repository') do |s|
+          options[:single] = s
+          options[:location_type] = :single
+        end
+
+        opts.on('-v', '--[no-]verbose', 'Runs verbosely') do |v|
+          options[:verbose] = v
+        end
+      end.parse!
+      options
+    end
+
+    def load_defs(options)
+      puts options.inspect
     end
   end
 end
