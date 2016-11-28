dd69b107ced36d57ebe2416111c611b030ad7822
Chris Rohr <crohr@nearinfinity.com>
2016-11-28 16:50:31 +0000

    Fixed items from review

--- lib/servitude/loaders/multi_git_loader.rb
+++ lib/servitude/loaders/multi_git_loader.rb
@@ -8,26 +8,30 @@ module Servitude
       puts 'Using Multi Git Loader' if options[:verbose]
 
       begin
-      # Make temp directory
-      Dir.mkdir('.servitude')
-
-      options[:group].each do |url|
-        puts "Pulling git repository of definitions from #{url}" if options[:verbose]
-        Dir.chdir('.servitude') do
-          `git clone #{url}`
+        # Make temp directory
+        Dir.mkdir('.servitude')
+  
+        options[:group].each do |url|
+          puts "Pulling git repository of definitions from #{url}" if options[:verbose]
+          Dir.chdir('.servitude') do
+            `git clone #{url}`
+          end
         end
+  
+        service_definitions = []
+  
+        Dir.glob('.servitude/**/SERVITUDE').each do |def_file|
+          puts "Loading definition #{def_file}" if options[:verbose]
+          
+          begin
+            service_definitions << Servitude::ServiceDefinition.from(YAML.load_file(File.join(def_file)))
+          rescue
+            puts "Failed to load #{def_file}.  Trying the rest."
+          end
+        end
+      ensure
+        FileUtils.rm_rf('.servitude')
       end
-
-      service_definitions = []
-
-      Dir.glob('.servitude/**/SERVITUDE').each do |def_file|
-        puts def_file
-        service_definitions << Servitude::ServiceDefinition.from(YAML.load_file(File.join(def_file)))
-      end
-
-    ensure
-      FileUtils.rm_r('.servitude')
-    end
 
       service_definitions
     end
